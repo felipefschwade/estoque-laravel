9aa0d790beaabb25ceab8bc9d116cc0ca55e49f4
Felipe Faé Schwade <felipefaeschwade@gmail.com>
2016-02-11 00:13:15 -0300

    Aula 5 - Atividade 2 - Modificando o nome do arquivo detalhes e incorporando o blade template

---
+++ resources/views/detalhes.blade.php
@@ -0,0 +1,16 @@
+@extends('principal');
+
+@section('conteudo')
+	<h1>Detalhes do Produto <?= $produto->nome ?></h1>
+	<ul>
+		<li>
+			Valor: <?= $produto->valor?>
+		</li>
+		<li>
+			Descrição: <?= $produto->descricao?>
+		</li>
+		<li>
+			Quantidade em Estoque: <?= $produto->quantidade?>
+		</li>
+	</ul>
+@stop
