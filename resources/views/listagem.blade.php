cf6ca64cefa991614b007ba09ae71c458bfdb519
Felipe Faé Schwade <felipefaeschwade@gmail.com>
2016-02-11 00:31:36 -0300

    Aula 5 - Atividade 5 - Adicionando um if para verificação dos produtos cadastrados utilizando o blade

--- resources/views/listagem.blade.php
+++ resources/views/listagem.blade.php
@@ -1,17 +1,23 @@
 @extends('principal')
 @section('conteudo')
 	<h1>Controle de Estoque</h1>
-	<table class="table">
-		@foreach($produtos as $p)
-			<tr>
-            	<td><?=$p->nome?></td>
-            	<td><?=$p->descricao?></td>
-            	<td><?=$p->valor?></td>
-            	<td><?=$p->quantidade?></td>
-        		<td><a href="/produtos/mostra/<?=$p->id?>">
-					  <span class="glyphicon glyphicon-search"></span>
-					</a>
-				</td>	
-            </tr>
-	    @endforeach
+	@if(empty($produtos))
+		<div class="alert alert-danger">
+			Você não possui nenhum produto cadastrado!
+		</div>
+	@else
+		<table class="table">
+			@foreach($produtos as $p)
+				<tr>
+	            	<td><?=$p->nome?></td>
+	            	<td><?=$p->descricao?></td>
+	            	<td><?=$p->valor?></td>
+	            	<td><?=$p->quantidade?></td>
+	        		<td><a href="/produtos/mostra/<?=$p->id?>">
+						  <span class="glyphicon glyphicon-search"></span>
+						</a>
+					</td>	
+	            </tr>
+		    @endforeach
+	@endif
 @stop
