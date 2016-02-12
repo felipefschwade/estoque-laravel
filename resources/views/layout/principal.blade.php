88fa8d47b43a39d28d3b6cd2e5ef742f3f6b4fbc
Felipe Faé Schwade <felipefaeschwade@gmail.com>
2016-02-11 23:47:40 -0300

    Aperfeiçoando a Estilização do layout

--- resources/views/layout/principal.blade.php
+++ resources/views/layout/principal.blade.php
@@ -1,8 +1,11 @@
 <html>
-<head>
-	<link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
-	<title>Estoque de Produtos</title>
-</head>
-	@yield('conteudo')
-</body>
+	<head>
+		<link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
+		<title>Estoque de Produtos</title>
+	</head>
+	<body>
+		<div class="container-fluid">
+			@yield('conteudo')
+		</div>
+	</body>
 </html>
