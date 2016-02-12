295e21715d2f36f8e8e038d0e526be969dd903b4
Felipe Faé Schwade <felipefaeschwade@gmail.com>
2016-02-12 00:05:52 -0300

    Mais correções da Estilização

--- resources/views/layout/principal.blade.php
+++ resources/views/layout/principal.blade.php
@@ -2,18 +2,27 @@
 	<head>
 		<link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
 		<title>Estoque de Produtos</title>
+		<style type="text/css">
+			body {
+				padding-top: 70px;
+			}
+		</style>
 	</head>
 	<body>
-		<header>
-			<ul class="nav navbar-nav navbar-right">
-			  <li><a href="/produtos">Listagem</a></li>
-			  <li><a href="/produtos/novo">Novo</a></li>
-			</ul>
-		</header>
-		<main>
-			<div class="container-fluid">
-				@yield('conteudo')
-			</div>
-		</main>	
+		<div class="container-fluid">
+			<header>
+				<nav class="navbar navbar-default navbar-fixed-top">
+				<div class="container">
+					<ul class="nav navbar-nav">
+						<li><a href="/produtos">Listagem</a></li>
+					  	<li><a href="/produtos/novo">Novo</a></li>
+					</ul>
+				</div>
+				</nav>
+			</header>
+			<main>
+					@yield('conteudo')
+			</main>
+		</div>	
 	</body>
 </html>
