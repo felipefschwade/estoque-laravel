21f23dfd10e5b121706fa225f4c157620a522623
Felipe Faé Schwade <felipefaeschwade@gmail.com>
2016-02-16 00:20:06 -0300

    Laravel Parte 2 - Aula 1 - Atividade 4 - Alterando o método mostra para utilizar o Eloquent

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -8,13 +8,12 @@
 			$produtos = Produto::all();
 			return view('produto/listagem')->with('produtos', $produtos);
 		}
-		public function mostra() {
-			$id = Request::route('id');
-			$produto = DB::select('select * from produtos where id = ?', [$id]);
+		public function mostra($id) {
+			$produto = Produto::find($id);
 			if (empty($produto)) {
 				echo "Este Produto não Existe!";
 			} else {
-				return view('produto/detalhes')->with('produto', $produto[0]);
+				return view('produto/detalhes')->with('produto', $produto);
 			}
 		}
 		public function novo() {
@@ -28,7 +27,7 @@
 			DB::insert('insert into produtos (nome, valor, quantidade, descricao) values (?,?,?,?)', array($nome, $valor, $quantidade, $descricao) );
 			return redirect()->action('ProdutoController@lista')->withInput(Request::only('nome'));
 		}
-		public function listaJSON(){
+		public function listaJSON() {
 			$produtos = DB::select('select * from produtos');
 			return response()->json($produtos);
 		}
