100f48a047c118a2faf4534169454422f0da6fde
Felipe Faé Schwade <felipefaeschwade@gmail.com>
2016-02-23 13:29:08 -0400

    Laravel Parte 2 - Aula 5 - Atividade 2 - Adicionando o nome à categoria e executando a migração

--- database/migrations/2016_02_23_171954_create_categorias_table.php
+++ database/migrations/2016_02_23_171954_create_categorias_table.php
@@ -3,26 +3,17 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCategoriasTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+class CreateCategoriasTable extends Migration {
+
     public function up()
     {
         Schema::create('categorias', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('nome');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::drop('categorias');
