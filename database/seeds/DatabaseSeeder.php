f5d31042d3df82b6eb344d62e13e439c077e9296
Felipe Faé Schwade <felipefaeschwade@gmail.com>
2016-02-23 22:41:16 -0400

    Laravel Parte 2 - Aula 6 - Atividade 1 - Preenchendo o DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,16 +1,11 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use estoque\Categoria;
 
-class DatabaseSeeder extends Seeder
+class DatabaseSeeder extends Seeder 
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // $this->call(UserTableSeeder::class);
+    public function run() {
+        $this->call(CategoriaTableSeeder::class);
     }
 }
